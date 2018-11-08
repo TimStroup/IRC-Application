6104c06321aeb55a3d864c6542c5e573a8222c6d
TimStroup <tstroup@rams.colostate.edu>
2018-11-08 01:01:08 -0700

    basic join command and ping/pong command. Updated compile command as well

--- channel.h
+++ channel.h
@@ -7,14 +7,18 @@ using namespace std;
 class channel
 {
     public: 
-    channel(string name);
+    channel(string name, string password);
     string getName();
+    string getPassword();
     void setTopic(string topic);
     void addUser(User *user);
-    void removeUser(string nickname);
+    void removeUser(string nickName);
+    void sendMessage(string message);
+    User* getUser(string nickName);
 
     private:
     string name;
     string topic;
+    string password;
     vector<User *> channelParticipants;
 };
