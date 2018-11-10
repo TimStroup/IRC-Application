3738e66d81ee05b090ecd3b66baba6ae370ebb36
TimStroup <tstroup@rams.colostate.edu>
2018-11-10 16:45:32 -0700

    finished registration and login

--- User.h
+++ User.h
@@ -8,6 +8,7 @@ class User
     public:
         User();
         User(const string nick, shared_ptr<cs457::tcpUserSocket> socket);
+        enum Level {ADMIN,SYSOP,USER,CHANNELOP};
         void setNick(const string nick);
         void setPass(const string pass);
         bool checkPass(const string passAttempt);
@@ -16,14 +17,20 @@ class User
         void setAwayStatus(bool);
         void setAwayMessage(string);
         bool getAwayStatus();
+        void setLevel(Level);
+        Level getLevel();
         string getAwayMessage();
         cs457::tcpUserSocket& getTcpUserSocket();
         cs457::tcpUserSocket *socketConnection;
-
+        
+        
     
     private:
         string nickName;
         string password;
         string awayMessage;
+        bool isBanned;
         bool awayStatus = false;
+        Level accountLevel = USER;
+        
 };
